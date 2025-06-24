80153fc80ed698fdde194fa8e2b96ccb8945e8cd
Divyang <Divyangdankhara007@outlook.com>
2025-06-24 10:28:39 -0400

    fix github link

--- src/data/social.ts
+++ src/data/social.ts
@@ -3,7 +3,7 @@ import type { SocialLink } from "../types/social";
 const socialLinks: SocialLink[] = [
   { title: 'GitHub', url: 'https://github.com/divyangdankhara' },
   { title: 'LinkedIn', url: 'https://www.linkedin.com/in/divyangdankhara' },
-  { title: 'Twitter', url: 'https://twitter.com/divyangdankhara' }
+  { title: 'Twitter', url: 'https://twitter.com/DankharaDivyang' }
 ];
 
 export default socialLinks;
