f0f114abce62a541b65fc7553487c1d474870a7c
Divyang <Divyangdankhara007@outlook.com>
2025-06-24 12:25:01 -0400

    fixing build issue.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/divyangdankhara.com/', // Use '/' for custom domains like divyangdankhara.com
+  base: '', // ✅ ← must be JUST a slash
 });
